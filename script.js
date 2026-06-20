b17cb780f90ac49a6bfdbaba250654ffcf2b77b0
walidtghz1997-source <walidtghz1997@gmail.com>
2026-06-20 20:10:56 +0100

    I created an function save users. json.

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 let users = JSON.parse(localStorage.getItem("bankUsers")) || [];
 let currentUser = null;
 let isRunning = true;
+
+mainMenu();
+
+function saveUsers() {
+    localStorage.setItem("bankUsers", JSON.stringify(users));
+}
