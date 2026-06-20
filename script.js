bb85db8c775d5462189db6f413f00c84073349a8
walidtghz1997-source <walidtghz1997@gmail.com>
2026-06-20 20:12:15 +0100

    I created an function main menu.

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ mainMenu();
 function saveUsers() {
     localStorage.setItem("bankUsers", JSON.stringify(users));
 }
+
+// this is main menu
+function mainMenu() {
+    let choice = "";
+
+    while (choice !== "4" && isRunning) {
+        choice = prompt(
+            "Bank Menu:\n" +
+            "1 - Sign Up\n" +
+            "2 - Login\n" +
+            "3 - Change Password\n" +
+            "4 - Exit"
+        );
+
+        if (choice === "1") {
+            signUp();
+        } else if (choice === "2") {
+            login();
+        } else if (choice === "3") {
+            changePassword();
+        } else if (choice === "4") {
+            isRunning = false;
+            alert("Goodbye");
+        } else {
+            alert("Invalid choice");
+        }
+    }
+}
